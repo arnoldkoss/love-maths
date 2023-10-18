9d7a122e6473d60ac6403e9829e0d2cb2d8aea03
arnoldkoss <>
2023-10-18 16:36:37 +0000

    add code ti increment score and wrong answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,10 +49,12 @@ function checkAnswer() {
     let calculatedAnswer = calculateCorrectAnswer();
     let isCorrect = userAnswer === calculatedAnswer[0];
     if(isCorrect){
-        alert("Hey, you got it right!")
+        alert("Hey, you got it right!");
+        incrementScore();
     }
     else{
         alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
     runGame(calculatedAnswer[1]);
 }
@@ -74,12 +76,21 @@ let operand2 = parseInt(document.getElementById("operand2").innerText);
     }
 }
 
-function incrementScore() {
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 
+function incrementScore() {
+   let oldScore = parseInt(document.getElementById("score").innerText);
+   document.getElementById("score").innerText = ++oldScore;
 }
 
+/**
+ * Gets the current tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
-
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
